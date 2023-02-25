d18855ab232eea88358650832c8e69b07743cd65
Raja Jawahar <rajajawahar77@gmail.com>
2023-02-25 18:47:51 +0530

    Minor correction in ViewModel pattern

--- app/src/main/java/com/example/mvvmcleanarchitecture/presentation/movie/MovieViewModel.kt
+++ app/src/main/java/com/example/mvvmcleanarchitecture/presentation/movie/MovieViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.mvvmcleanarchitecture.presentation.movie
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import com.example.mvvmcleanarchitecture.domain.usecase.GetMoviesUseCase
 import com.example.mvvmcleanarchitecture.domain.usecase.UpdateMoviesUseCase
 
@@ -8,5 +9,15 @@ class MovieViewModel(
   val getMoviesUseCase: GetMoviesUseCase,
   val updateMoviesUseCase: UpdateMoviesUseCase
 ) : ViewModel() {
+
+  fun getMovies() = liveData {
+    val movieList = getMoviesUseCase.execute()
+    emit(movieList)
+  }
+
+  fun updateMovies() = liveData {
+    val movieList = updateMoviesUseCase.execute()
+    emit(movieList)
+  }
 
 }
